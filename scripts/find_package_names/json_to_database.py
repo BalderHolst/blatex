7b37d1f050b307bf9867a48cc92c4f34dac0f636
Balder W. Holst <balderwh@gmail.com>
2023-02-01 23:50:58 +0100

    Add common column to database

--- scripts/find_package_names/json_to_database.py
+++ scripts/find_package_names/json_to_database.py
@@ -55,17 +55,35 @@ def cash_tex_package_counts(db: Database):
         texlive_package["nr_of_tex_packages"] = blatex.packages.get_number_of_tex_packages(db, texlive_package['name'])
         db.update_entry(texlive_package)
 
-def add_common_column(db: Database, file: Path):
+def add_common_column(db: Database):
 
     colname = "common"
-    if not db.is_column("texlive_packages", colname):
-        db.add_column("texlive_packages", colname)
+    table = "texlive_packages"
+    if not db.is_column(table, colname):
+        db.add_column(table, Column(colname, "bool", default_value=False))
 
-    with file.open("r") as f:
+    with Path("scripts/find_package_names/common_texlive_packages.txt").open("r") as f:
         common_packages = f.readlines()
 
     for common_package in common_packages:
-        print(common_package)
+        common_package = common_package[:-1]
+
+        print(f"Setting common flag for {common_package!r}")
+
+        res = db.SELECT().FROM(table).WHERE("name", common_package).run()
+
+        if not res:
+            print(f"ERROR: could not find package with the name: {common_package!r}")
+            continue
+
+        entry = res[0]
+
+        entry["common"] = True
+
+        db.update_entry(entry)
+
+
+        
 
 
 def json_to_database():
@@ -94,8 +112,13 @@ def json_to_database():
     print("Cashing number of tex packages in every texpackage")
     cash_tex_package_counts(db)
 
+    add_common_column(db)
+
     db.close()
 
 
 if __name__ == "__main__":
-    json_to_database()
+    db = Database(db_file)
+    add_common_column(db)
+
+    db.close()
