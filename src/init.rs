424f98c73f8567d976f8be71e87e2bfb7c43d09d
BalderHolst <balderwh@gmail.com>
2023-11-01 19:30:56 +0100

    Add remote templates to init list

--- src/init.rs
+++ src/init.rs
@@ -1,23 +1,44 @@
-use std::{fs, io::Cursor, path::PathBuf};
+use std::{fs, io::Cursor, path::PathBuf, collections::HashMap};
 
 use fuzzy_finder::item::Item;
 
 use crate::{
     config::{self, LOCAL_CONFIG_FILE},
-    opts::Config,
+    opts::{Config, RemoteTemplate},
 };
 
-fn get_templates<P>(dir: P) -> Vec<PathBuf>
+enum Template {
+    Local(PathBuf),
+    Remote(String, RemoteTemplate),
+}
+
+fn get_templates<P>(templates_dir: P, remote_templates: HashMap<String, RemoteTemplate>) -> Vec<Template>
+where
+    P: AsRef<std::path::Path>,
+{
+    // Get the local templates
+    let mut templates: Vec<Template> = get_local_templates(templates_dir)
+        .iter()
+        .map(|t| Template::Local(t.clone()))
+        .collect();
+
+    // Insert the remote templates
+    templates.extend(remote_templates.iter().map(|(n, t)| Template::Remote(n.clone() + " (remote)", t.clone())));
+
+    return templates;
+}
+
+fn get_local_templates<P>(templates_dir: P) -> Vec<PathBuf>
 where
     P: AsRef<std::path::Path>,
 {
     let mut templates = Vec::new();
-    for file in fs::read_dir(&dir).unwrap() {
+    for file in fs::read_dir(&templates_dir).unwrap() {
         let path = file.unwrap().path();
         if path.is_file() {
-            templates.push(dir.as_ref().join(path.file_name().unwrap()))
+            templates.push(templates_dir.as_ref().join(path.file_name().unwrap()))
         } else if path.is_dir() {
-            templates.extend(get_templates(path))
+            templates.extend(get_local_templates(path))
         }
     }
     templates
@@ -25,23 +46,31 @@ where
 
 pub fn init(cwd: PathBuf, config: Config, template: Option<String>) {
     let templates_dir = &config.templates_dir;
-    let templates = get_templates(templates_dir.as_path());
+    let templates = get_templates(templates_dir.as_path(), config.remote_templates);
 
     let template_path = match template {
-        Some(p) => templates_dir.join(p).with_extension("zip"),
+        Some(p) => {
+            // TODO: Handle remote templates
+            templates_dir.join(p).with_extension("zip")
+        },
         None => {
             // Create fuzzy finder items
-            let items: Vec<Item<&PathBuf>> = templates
+            let items: Vec<Item<&Template>> = templates
                 .iter()
-                .map(|p| {
-                    Item::new(
-                        p.strip_prefix(templates_dir.as_path())
-                            .unwrap()
-                            .to_str()
-                            .unwrap()
-                            .to_string(),
-                        p,
-                    )
+                .map(|t| {
+                    match t {
+                        Template::Local(p) => {
+                            Item::new(
+                                p.strip_prefix(templates_dir.as_path())
+                                .unwrap()
+                                .to_str()
+                                .unwrap()
+                                .to_string(),
+                                t,
+                                )
+                        },
+                        Template::Remote(name, _r) => Item::new(name.clone(), t),
+                    }
                 })
                 .collect();
 
@@ -53,13 +82,13 @@ pub fn init(cwd: PathBuf, config: Config, template: Option<String>) {
 
             // Run the fuzzy finder
             match fuzzy_finder::FuzzyFinder::find(items, nr_of_items as i8).unwrap() {
-                Some(p) => p,
+                Some(Template::Local(p)) => p.to_path_buf(),
+                Some(Template::Remote(name, remote)) => todo!(),
                 None => {
                     eprintln!("No template chosen.");
                     std::process::exit(1);
                 }
             }
-            .to_path_buf()
         }
     };
 
