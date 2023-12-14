166bad3c239509e80e626277b309a5fca5675624
BalderHolst <balderwh@gmail.com>
2023-12-14 14:11:01 +0100

    Factor out man page generation

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use clap::CommandFactory;
+use clap::{Command, CommandFactory};
 
 #[path = "src/opts.rs"]
 mod opts;
@@ -12,14 +12,19 @@ mod utils;
 mod config;
 
 fn main() -> std::io::Result<()> {
-    let out_dir = std::path::PathBuf::from("man");
+    let out_man_dir = env!("CARGO_MANIFEST_DIR").to_string() + "/man";
 
-    let cmd = opts::Args::command();
+    create_man_page("blatex.1", opts::Args::command(), out_man_dir.as_str())?;
+
+    Ok(())
+}
+
+fn create_man_page(filename: &str, cmd: Command, out_dir: &str) -> std::io::Result<()> {
     let man = clap_mangen::Man::new(cmd);
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)?;
 
-    std::fs::write(out_dir.join("blatex.1"), buffer)?;
+    std::fs::write(out_dir.to_string() + "/" + filename, buffer)?;
 
     Ok(())
 }
